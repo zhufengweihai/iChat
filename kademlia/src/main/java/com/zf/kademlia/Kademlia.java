cab158722b7ab4dc27b1da6ddf644a65aa1d4451
zhufengweihai <zhufengweihai@gmail.com>
2017-12-05 21:52:16 +0800

    commit code

--- kademlia/src/main/java/com/zf/kademlia/Kademlia.java
+++ kademlia/src/main/java/com/zf/kademlia/Kademlia.java
@@ -1,13 +1,17 @@
 package com.zf.kademlia;
 
+import com.zf.kademlia.node.Key;
 import com.zf.kademlia.node.Node;
 import com.zf.kademlia.operation.FindNodeOperation;
 import com.zf.kademlia.operation.PingOperation;
+import com.zf.kademlia.operation.StoreOperation;
 import com.zf.kademlia.routing.RoutingTable;
 
 import org.greenrobot.eventbus.EventBus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * @author zhufeng7
@@ -15,7 +19,6 @@ import org.slf4j.LoggerFactory;
  */
 public class Kademlia {
     private static final Logger LOGGER = LoggerFactory.getLogger(Kademlia.class);
-    private RoutingTable routingTable = null;
 
     private void init() {
         EventBus.getDefault().register(this);
@@ -24,6 +27,21 @@ public class Kademlia {
     public void bootstrap(Node bootstrapNode) {
         new PingOperation(bootstrapNode).execute();
         new FindNodeOperation(bootstrapNode, KadDataManager.instance().getLocalNode().getId()).execute();
+        refreshBuckets();
+    }
+
+    public void store(Key key, String value) {
+
+    }
+
+    private void refreshBuckets() {
+        for (int i = 1; i < Key.ID_LENGTH; i++) {
+            final Key current = KadDataManager.instance().getLocalNode().getId().generateNodeIdByDistance(i);
+            List<Node> nodes = KadDataManager.instance().getRoutingTable().getNodes();
+            for (Node node : nodes) {
+                new FindNodeOperation(node,current).execute();
+            }
+        }
     }
 
     public void close() {
